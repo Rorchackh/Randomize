9ac36dec2e62c1fb3770b1077776669586b48ba3
Ayoub Khobalatte <ayoub.khobalatte@gmail.com>
2013-02-17 01:05:57 +0100

    adding polyfill hack

--- Randomize.js
+++ Randomize.js
@@ -29,9 +29,24 @@ var Randomizer = {
             return a.join("");
         }
 
-        /* Polyfill hack might be needed later */
+        if (!Object.create) {
+            Object.create = function (o) {
+                if (arguments.length > 1) {
+                    throw new Error('Object.create implementation only accepts the first parameter.');
+                }
+                function F() {}
+                F.prototype = o;
+                return new F();
+            };
+        }
+
+        return this;
     },
     randomize: function () {
+
+        if (!Array.prototype.shuffle) {
+            this.init();
+        }
 
         html = this.getSelectionHtml();
 
@@ -103,6 +118,5 @@ var Randomizer = {
 
 function test() {
     var r = Object.create(Randomizer);
-    r.init();
-    r.randomize();    
+    r.init().randomize;
 }
